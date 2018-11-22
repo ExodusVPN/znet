82dc1ef7af0c9cce717bbc350bd16e13b0b2f90d
luozijun <luozijun.assistant@gmail.com>
2018-11-23 01:12:12 +0800

    update examples/dns.rs

--- examples/dns.rs
+++ examples/dns.rs
@@ -2,13 +2,10 @@ extern crate net2;
 
 fn main (){
     println!("{:?}", net2::dns::get_network_global());
-
-
+    
     for service in net2::dns::list_network_services_order() {
         println!("{:?}\n\n", service);
     }
     
-
     println!("{:?}", net2::dns::list_network_interfaces());
-
 }
