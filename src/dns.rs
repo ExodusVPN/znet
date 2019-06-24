3103ff10dee307e03221ed007902b6fe8b2e4416
luozijun <luozijun.assistant@gmail.com>
2019-06-24 15:28:24 +0800

    update src/dns.rs

--- src/dns.rs
+++ src/dns.rs
@@ -512,7 +512,37 @@ mod platform {
 
 #[cfg(target_os = "linux")]
 mod platform {
-    // file: /etc/resolv.conf
+    use std::fs::{File, OpenOptions};
+    use std::io::{Read, Write};
+    use std::net::IpAddr;
+    
+    pub fn get_default_dns() -> Option<Vec<IpAddr>> {
+        // file: /etc/resolv.conf
+        match File::open("/etc/resolv.conf") {
+            Ok(mut file) => {
+                let mut ips = Vec::new();
+
+                let mut contents = String::new();
+                file.read_to_string(&mut contents).ok()?;
+                for line in contents.lines() {
+                    if line.starts_with("nameserver") {
+                        for tmp in line.trim().split(" ") {
+                            if let Ok(ip) = tmp.parse::<IpAddr>() {
+                                ips.push(ip);
+                            }
+                        }
+                    }
+                }
+
+                if ips.len() > 0 {
+                    return Some(ips);
+                } else {
+                    return None;
+                }
+            },
+            Err(_) => None,
+        }
+    }
 }
 
 
